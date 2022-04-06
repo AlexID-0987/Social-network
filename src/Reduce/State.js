0d721c550fb13d656baec43e0e494b519448bd00
Olexiy <a1980dominov@gmail.com>
2022-04-06 18:39:30 +0300

    Continue flux

--- src/Reduce/State.js
+++ src/Reduce/State.js
@@ -1,33 +1,54 @@
-import { renderproject } from "../Render"
-
-let state={
-    Statebody:{
-        Peoples: [
-            { id: 2, name: 'Alex' },
-            { id: 5, name: 'Oleg' },
-            { id: 45, name: 'Helga' }
-        ],
-        Arraymessage: [
-            { id: 1, mess: 'Hello I a`m UKRAINE' },
-            { id: 4, mess: 'Hello' },
-            { id: 67, mess: 'I love live' }
-        ],
-        Array: [
-            { item: 'Hello' },
-            { item: 'I Love' },
-            { item: 'Good morning' }
-        ],
-        NewPostText:'Hello'
+let store={
+    _state:{
+        Statebody:{
+            Peoples: [
+                { id: 2, name: 'Alex' },
+                { id: 5, name: 'Oleg' },
+                { id: 45, name: 'Helga' }
+            ],
+            Arraymessage: [
+                { id: 1, mess: 'Hello I a`m UKRAINE' },
+                { id: 4, mess: 'Hello' },
+                { id: 67, mess: 'I love live' }
+            ],
+            Array: [
+                { item: 'Hello' },
+                { item: 'I Love' },
+                { item: 'Good morning' }
+            ],
+            NewPostText:'Hello'
+        }
+    },
+    getstate(){
+        return this._state;
+    },
+    renderproject() {
+        console.log('State setstate')
+    },
+    Subscribe(observer){
+        this.renderproject=observer
+    },
+    pushitem(){
+        let addit = { item:this._state.Statebody.NewPostText }
+        this._state.Statebody.Array.push(addit)
+        this._state.Statebody.NewPostText=''
+        this.renderproject(this._state)
+    },
+    Updatepost(newpost){
+        this._state.Statebody.NewPostText=newpost;
+        this.renderproject(this._state)
     }
+    
 }
-export let pushitem=()=>{
-    let addit = { item: state.Statebody.NewPostText }
-    state.Statebody.Array.push(addit)
-    state.Statebody.NewPostText=''
-    renderproject(state)
-}
-export let Updatepost=(newpost)=>{
-    state.Statebody.NewPostText=newpost;
-    renderproject(state)
-}
-export default state
+        
+    
+     
+    
+    
+
+
+
+
+
+export default store
+window.store=store;
--- src/index.js
+++ src/index.js
@@ -1,8 +1,26 @@
 import './index.css';
-import state from './Reduce/State';
-import {renderproject} from './Render'
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './index.css';
+import App from './App';
+import { BrowserRouter } from 'react-router-dom';
+import store from './Reduce/State'
 
-
-renderproject(state)
+let renderproject=(state)=>{
+  ReactDOM.render(
+    <>
+    <BrowserRouter>
+        <App Peoples={state.Statebody.Peoples}
+          Arraymessage={state.Statebody.Arraymessage}
+          Arr={state.Statebody.Array} Addpost={store.pushitem.bind(store)} 
+          Newtext={state.Statebody.NewPostText}
+          updatepost={store.Updatepost.bind(store)}/>
+      </BrowserRouter>
+    </>,
+    document.getElementById('root')
+  );
+}
+renderproject(store.getstate())
+store.Subscribe(renderproject)
 
 
